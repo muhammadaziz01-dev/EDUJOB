085b33139c629628efef65832b84e44d3084f36e
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-03-21 09:27:41 +0300

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,21 +5,21 @@
 // ----------LOADER JS START--------------------------------
 let loadWrapper = document.querySelector('.load_wrapper');
 
-// window.addEventListener('DOMContentLoaded',()=>{
-//     loadWrapper.style.display = 'flex';
-//     document.body.style.overflow="hidden";
-// });
+window.addEventListener('DOMContentLoaded',()=>{
+    loadWrapper.style.display = 'flex';
+    document.body.style.overflow="hidden";
+});
 
-// window.addEventListener('load',()=>{
+window.addEventListener('load',()=>{
 
-//     setTimeout( ()=>{
+    setTimeout( ()=>{
 
-//         loadWrapper.style.display = 'none';
-//         document.body.style.overflow="visible";
+        loadWrapper.style.display = 'none';
+        document.body.style.overflow="visible";
 
-//     },1000)
+    },1000)
   
-// });
+});
 // ----------LOADER JS END--------------------------------
 
 
@@ -30,16 +30,19 @@ let  caruselWrap = document.querySelector(".carusel2");
 let caruselLength= document.querySelectorAll(".span-wrap-span")
 
 let caruselCards =document.querySelectorAll('.carusel2-card');
-// let sum = 0;
+let sum = 0;
 let indx = 0;
 
 function caruselRight() {
     indx ++ ;
+    
+    
     if(indx > caruselCards.length-3){
-        // sum  = 0;
+        
         indx = 0;
+        sum = 0;
     }else if(indx < 0){
-        // sum = caruselCards.length-3;
+       
         indx = caruselCards.length-3;
     }
 
@@ -51,7 +54,77 @@ function caruselRight() {
     // }else{
     //     sum++;
     // }
-    // console.log(sum);
+    if(sum == 0 ){
+        caruselLength[0].style.backgroundColor = "#0A65CC"; 
+        caruselLength[0].style.width = "17px"; 
+
+        caruselLength[1].style.backgroundColor = "#99C2FF"; 
+        caruselLength[1].style.width = "10px"; 
+        caruselLength[2].style.backgroundColor = "#99C2FF"; 
+        caruselLength[2].style.width = "10px";
+        caruselLength[3].style.backgroundColor = "#99C2FF"; 
+        caruselLength[3].style.width = "10px";
+        caruselLength[4].style.backgroundColor = "#99C2FF"; 
+        caruselLength[4].style.width = "10px";
+        sum ++ ;
+
+    }else if( sum == 1 ){
+        caruselLength[1].style.backgroundColor = "#0A65CC"; 
+        caruselLength[1].style.width = "17px";
+
+        caruselLength[0].style.backgroundColor = "#99C2FF"; 
+        caruselLength[0].style.width = "10px"; 
+        caruselLength[2].style.backgroundColor = "#99C2FF"; 
+        caruselLength[2].style.width = "10px"; 
+        caruselLength[3].style.backgroundColor = "#99C2FF"; 
+        caruselLength[3].style.width = "10px";
+        caruselLength[4].style.backgroundColor = "#99C2FF"; 
+        caruselLength[4].style.width = "10px";
+        sum ++ ;
+
+    }else if(sum==2){
+        caruselLength[2].style.backgroundColor = "#0A65CC";
+        caruselLength[2].style.width = "17px"; 
+
+        caruselLength[0].style.backgroundColor = "#99C2FF"; 
+        caruselLength[0].style.width = "10px"; 
+        caruselLength[1].style.backgroundColor = "#99C2FF"; 
+        caruselLength[1].style.width = "10px"; 
+        caruselLength[3].style.backgroundColor = "#99C2FF"; 
+        caruselLength[3].style.width = "10px";
+        caruselLength[4].style.backgroundColor = "#99C2FF"; 
+        caruselLength[4].style.width = "10px";
+        sum ++ ;
+
+    } else if(sum==3){
+        caruselLength[3].style.backgroundColor = "#0A65CC";
+        caruselLength[3].style.width = "17px"; 
+
+        caruselLength[0].style.backgroundColor = "#99C2FF"; 
+        caruselLength[0].style.width = "10px"; 
+        caruselLength[2].style.backgroundColor = "#99C2FF"; 
+        caruselLength[2].style.width = "10px"; 
+        caruselLength[1].style.backgroundColor = "#99C2FF"; 
+        caruselLength[1].style.width = "10px";
+        caruselLength[4].style.backgroundColor = "#99C2FF"; 
+        caruselLength[4].style.width = "10px";
+        sum ++;
+    }else if(sum == 4){
+        caruselLength[4].style.backgroundColor = "#0A65CC";
+        caruselLength[4].style.width = "17px"; 
+
+        caruselLength[0].style.backgroundColor = "#99C2FF"; 
+        caruselLength[0].style.width = "10px"; 
+        caruselLength[2].style.backgroundColor = "#99C2FF"; 
+        caruselLength[2].style.width = "10px"; 
+        caruselLength[1].style.backgroundColor = "#99C2FF"; 
+        caruselLength[1].style.width = "10px";
+        caruselLength[3].style.backgroundColor = "#99C2FF"; 
+        caruselLength[3].style.width = "10px";
+        sum ++;
+    }
+
+
     caruselWrap.style.transform = `translateX(${-indx * 465}px)`;
     
 }
@@ -67,4 +140,38 @@ function caruselLeft() {
     console.log("left carusel");
     caruselWrap.style.transform = `translateX(${-indx * 465}px)`;
 }
+
+const tim = setInterval(()=>{
+    caruselRight();
+},3000)
 // --------------CARUEL END --------------------------------
+
+
+
+// ---------------SELECTORS --------------------------------
+let select1 = document.querySelector("#select1");
+let select2 = document.querySelector("#select2");
+let imgSelect1 = document.querySelector('#img-select1');
+let imgSelect2 = document.querySelector('#img-select2');
+select1.addEventListener("change", (e)=>{
+    console.log(e);
+    if(e.target.value == "English"){
+        imgSelect1.src="./assets/img/flag-usa.svg"
+        console.log("Enabling");
+    }else if(e.target.value == "Indian"){
+        console.log('Indonesian');
+        imgSelect1.src="./assets/img/flag-ind.svg"
+    }
+});
+
+
+select2.addEventListener("change", (e)=>{
+    console.log(e);
+    if(e.target.value == "USA"){
+        imgSelect2.src="./assets/img/flag-usa.svg"
+        console.log("Enabling");
+    }else if(e.target.value == "India"){
+        console.log('India');
+        imgSelect2.src="./assets/img/flag-ind.svg"
+    }
+});
